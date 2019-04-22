5bb7119394ea3c0bf5d61cf3ceadffa8d80ef544
Shilpa k <shilpasiva1510@gmail.com>
2019-04-22 12:40:07 +0530

    Material-UI tables

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 class App extends Component {
   constructor(props) {
@@ -22,9 +30,7 @@ class App extends Component {
             user: result
           });
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
+        
         (error) => {
           this.setState({
             isLoaded: true,
@@ -35,24 +41,42 @@ class App extends Component {
   }
 
   render() {
+    var tableStyle = {
+      // minWidth: 200
+    }
+    var paperStyle = {
+      margin: 40,
+    }
+
     const { error, isLoaded, user } = this.state;
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
-      return (
-        <ul>
-          {user.map(user => (
-            <li key={user.id}>
-              {user.name} ,{user.phone}
-            </li>
-          ))}
-        </ul>
-      );
+        return (
+          <Paper style={paperStyle}>
+            <Table style={tableStyle}>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell>phone</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {user.map(user => (
+                  <TableRow key={user.id}>
+                    <TableCell>{user.name}</TableCell>
+                    <TableCell>{user.phone}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Paper>
+        );
+      }
     }
   }
-}
 
 
 export default App;
