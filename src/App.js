9fca3bc367e155f3da1035d5dfc240a9fb5095bc
Shilpa k <shilpasiva1510@gmail.com>
2019-04-22 17:22:35 +0530

    Modal

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Modal from '@material-ui/core/Modal';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   constructor(props) {
@@ -16,7 +19,8 @@ class App extends Component {
     this.state = {
       error: null,
       isLoaded: false,
-      user: []
+      user: [],
+      open: false
     };
   }
 
@@ -40,12 +44,37 @@ class App extends Component {
       )
   }
 
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
   render() {
     var tableStyle = {
       // minWidth: 200
     }
     var paperStyle = {
       margin: 40,
+    }
+    var modalStyle = {
+      top: '10%',
+      left: '10%'
+
+    }
+
+    var paper = {
+      width: '50%',
+      height: '80%',
+      backgroundColor: "white",
+      // outline: 'none',
+      // 'flex-direction': 'row',
+      // 'justify-content': 'center',
+      // 'flex-wrap': 'nowrap',
+      // 'align-items': 'stretch',
+      // 'align-content': 'stretch',
     }
 
     const { error, isLoaded, user } = this.state;
@@ -55,24 +84,39 @@ class App extends Component {
       return <div>Loading...</div>;
     } else {
         return (
-          <Paper style={paperStyle}>
-            <Table style={tableStyle}>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Name</TableCell>
-                  <TableCell>phone</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {user.map(user => (
-                  <TableRow key={user.id}>
-                    <TableCell>{user.name}</TableCell>
-                    <TableCell>{user.phone}</TableCell>
+          <div>
+            <Paper style={paperStyle}>
+              <Table style={tableStyle}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Name</TableCell>
+                    <TableCell>phone</TableCell>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </Paper>
+                </TableHead>
+                <TableBody>
+                  {user.map(user => (
+                    <TableRow key={user.id} onClick={this.handleOpen}>
+                      <TableCell>{user.name}</TableCell>
+                      <TableCell>{user.phone}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Paper>
+            <Modal 
+              key={user.id} 
+              style={modalStyle}
+              open={this.state.open}
+              onClose={this.handleClose}
+             
+            >   
+              <div style={paper}>
+              <Typography>
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.ggjgjhgjgjhgjgjhgjhgjg bjhjhg jhgjhgjhgj jghjgjg jghjg  gjgjgghghggjgg hhhhhhhhhhhhhhh
+              </Typography>
+              </div>
+            </Modal>
+          </div>
         );
       }
     }
